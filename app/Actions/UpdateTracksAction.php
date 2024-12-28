423d09f92729fe64168bf5084c9eb12a42a8da2b
dajamplay <ggyoug@gmail.com>
2024-12-28 15:29:35 +0300

    refactoring

--- app/Actions/UpdateTracksAction.php
+++ app/Actions/UpdateTracksAction.php
@@ -17,15 +17,7 @@ class UpdateTracksAction {
     public function run($file = null): array
     {
         if ($file === null) {
-
-            $files = Storage::files('public/music', true);
-
-            Track::truncate();
-            Storage::deleteDirectory('artwork');
-
-            foreach ($files as $file) {
-                $this->loadFile($file);
-            }
+            $this->fullUpdate();
         } else {
             $this->loadFile($file);
         }
@@ -38,62 +30,79 @@ class UpdateTracksAction {
         ];
     }
 
+    private function fullUpdate(): void
+    {
+        Track::truncate();
+        Storage::deleteDirectory('artwork');
+
+        $files = Storage::files('public/music', true);
+
+        foreach ($files as $file) {
+            $this->loadFile($file);
+        }
+    }
+
     public function loadFile($file): void
     {
-
-        $file_url_mp3 = Storage::url($file);
-
         try {
+            $file_url_mp3 = Storage::url($file);
             $audio = new Mp3Info(Storage::path($file), true);
-
             $file_url_jpg = str_replace('/storage/music', '/artwork', $file_url_mp3);
-
-            if($image_base64 = $audio->getCover()) {
-                $artworkIsSave = Storage::disk('public')->put($file_url_jpg  . '.jpg', $image_base64);
-
-                if ($artworkIsSave) {
-                    $artwork = '/storage' .$file_url_jpg  . '.jpg';
-                } else {
-                    $artwork = '/storage/default/no_artwork.jpg';
-                }
-
-            } else {
-                $artwork = '/storage/default/no_artwork.jpg';
-            }
+            $defaultTrackTitle = $this->getDefaultTitle($file_url_mp3);
 
             $track = new Track();
+
             $track->url = $file_url_mp3;
+            $track->title = $this->getTitle($audio, $defaultTrackTitle);
+            $track->artist = $this->getArtist($audio, $defaultTrackTitle);
+            $track->duration = $this->getDuration($audio);
+            $track->artwork = $this->getArtwork($file_url_jpg, $audio);
 
-            $defaultTrackTitleArray = explode('/', $file_url_mp3);
-            $defaultTrackTitle = array_pop($defaultTrackTitleArray);
-
-            if (array_key_exists('song', $audio->tags)) {
-                $track->title = trim($audio->tags['song'], '?');
-            } else {
-                $track->title = $defaultTrackTitle;
-            }
-
-            if (array_key_exists('artist', $audio->tags)) {
-                $track->artist = trim($audio->tags['artist'], '?');
-            } else {
-                $track->artist = $defaultTrackTitle;
-            }
-
-            $track->duration = $audio->duration;
-            $track->artwork = $artwork;
-
-            $isSave = $track->save();
-
-            if ($isSave) {
-                $this->trackUpdatedCount++;
-            } else {
-                $this->trackFailUpdatedCount++;
-            }
+            if ($track->save()) $this->trackUpdatedCount++;
 
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             $this->errors[] .= $e->getMessage();
+            $this->trackFailUpdatedCount++;
         }
+    }
+
+    private function getDuration($audio) {
+        return $audio->duration;
+    }
+
+    private function getTitle($audio, $defaultTrackTitle) {
+        if (array_key_exists('song', $audio->tags)) {
+            return trim($audio->tags['song'], '?');
+        }
+        return $defaultTrackTitle;
+    }
+
+    private function getArtist($audio, $defaultTrackTitle) {
+        if (array_key_exists('artist', $audio->tags)) {
+            return trim($audio->tags['artist'], '?');
+        }
+        return $defaultTrackTitle;
+    }
+
+    private function getDefaultTitle($file_url_mp3): ?string
+    {
+        $defaultTrackTitleArray = explode('/', $file_url_mp3);
+        return array_pop($defaultTrackTitleArray);
+    }
+
+    private function getArtwork($file_url_jpg, $audio): string
+    {
+        if($image_base64 = $audio->getCover()) {
+            $artworkIsSave = Storage::disk('public')
+                ->put($file_url_jpg  . '.jpg', $image_base64);
+
+            if ($artworkIsSave) {
+                return '/storage' .$file_url_jpg  . '.jpg';
+            }
+        }
+
+        return '/storage/default/no_artwork.jpg';
     }
 
 }
